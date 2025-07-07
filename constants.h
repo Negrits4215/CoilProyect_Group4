b71c459e601049a7cc5fec909539128aa4a1d227
Negrits4215 <115021032+Negrits4215@users.noreply.github.com>
2025-07-06 21:44:18 -0300

    Add files via upload

---
+++ constants.h
@@ -0,0 +1,23 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define SCREEN_WIDTH    50
+#define SCREEN_HEIGHT   20
+#define PIPE_GAP        6
+#define PIPE_DISTANCE   20
+#define NUM_PIPES       3
+
+#define GRAVITY         0.25f
+#define JUMP_STRENGTH  -1.5f
+#define MAX_FALL_SPEED   2.5f
+#define FRAME_TIME_MS   16
+
+typedef struct {
+    int x;
+    int gapY;
+    int passed;
+} Pipe;
+
+extern Pipe pipes[];
+
+#endif
